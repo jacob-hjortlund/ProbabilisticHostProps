e67070c7a138a5a8b96dce30bc2f9cae490ac5c1
Jacob O. Hjortlund <jacob.hjortlund@gmail.com>
2024-05-08 12:44:34 +0200

    typo

--- src/scripts/bash_scripts/pantheon_hubble_flow.sh
+++ src/scripts/bash_scripts/pantheon_hubble_flow.sh
@@ -1,5 +1,5 @@
 python src/scripts/reusable_scripts/preprocessing.py \
 run_name=pantheon_hubble_flow \
-data.name=Pantheon+SH0Es.dat \
+data.name=Pantheon+SH0ES.dat \
 filters.redshift.kwargs.z_min=0.0233 \
 filters.redshift.kwargs.z_max=0.15 \
